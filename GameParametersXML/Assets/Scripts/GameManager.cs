8ae886095b497e342d85c7ecf2e904ae6617183e
Raúl Serrano Gómez <raulserranodev@gmail.com>
2019-03-04 21:26:50 +0100

    1.0

---
+++ GameParametersXML/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Clase persistente entre escenas
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        #region Persistent_Singleton
+
+        public static GameManager Instance;
+
+        /// <summary>
+        /// Persistent Singleton
+        /// </summary>
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+
+        #endregion Persistent_Singleton
+
+        public GameParameters GameParameters;
+    }
+}
---
+++ GameParametersXML/Assets/Scripts/GameParameters.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// Contiene parámetros ajustables del juego
+    /// </summary>
+    [System.Serializable]
+    public class GameParameters
+    {
+        [Header("General")]
+        public int TimeCount;
+        public float DeathDuration;
+
+        [Header("Player")]
+        public float PlayerRunSpeed;
+        public float PlayerJumpHeight;
+        public int PlayerMeleeDamage;
+
+        [Header("Abilities")]
+        [Header("Fire")]
+        public int FireDamage;
+        public float FireCastDuration;
+        public float FireCastRate;
+
+        [Header("Ice")]
+        public int IceDamage;
+        public float IceCastDuration;
+        public float IceCastRate;
+
+        [Header("Enemies")]
+        [Header("Spider")]
+        public float SpiderRunSpeed;
+        public int SpiderMeleeDamage;
+    }
+}
